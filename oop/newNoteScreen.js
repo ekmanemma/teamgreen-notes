515d033a34f24db25957f546c61bc87b22dd32a1
Emma Ekman <emma.ekman@hotmail.com>
2020-01-13 12:36:21 +0100

    able to style in the form input

--- oop/newNoteScreen.js
+++ oop/newNoteScreen.js
@@ -13,6 +13,47 @@ class NewNoteScreen extends Screen {
         this.form = document.createElement('form');
         this.form.id = 'formNewNote';
         this.mainNotes.appendChild(this.form);
+
+        //adding element for styling
+        this.stylingNotes = document.createElement('div');
+        this.stylingNotes.id = 'stylingNotes';
+        this.form.appendChild(this.stylingNotes);
+
+        this.btnForBold = document.createElement('button');
+        this.btnForBold.textContent = 'B';
+        this.btnForBold.setAttribute('class', 'stylingButton');
+        this.btnForBold.setAttribute('type', 'button');
+
+        this.stylingNotes.appendChild(this.btnForBold);
+
+        this.btnForUnderline = document.createElement('button');
+        this.btnForUnderline.textContent = 'U';
+        this.btnForUnderline.setAttribute('class', 'stylingButton');
+        this.btnForUnderline.setAttribute('id', 'btnForUnderline');
+        this.btnForUnderline.setAttribute('type', 'button');
+        this.stylingNotes.appendChild(this.btnForUnderline);
+
+        this.btnForSmall = document.createElement('button');
+        this.btnForSmall.textContent = '14px';
+        this.btnForSmall.setAttribute('class', 'stylingButton');
+        this.btnForSmall.setAttribute('id', 'btnForSmall');
+        this.btnForSmall.setAttribute('type', 'button');
+        this.stylingNotes.appendChild(this.btnForSmall);
+
+        this.btnForBig = document.createElement('button');
+        this.btnForBig.textContent = '22px';
+        this.btnForBig.setAttribute('class', 'stylingButton');
+        this.btnForBig.setAttribute('id', 'btnForBig');
+        this.btnForBig.setAttribute('type', 'button');
+        this.stylingNotes.appendChild(this.btnForBig);
+
+        
+
+        // this.btnForBold.addEventListener('click', (e) =>{
+        //     this.contentSpan = document.getElementsByTagName('span');
+        //     this.contentSpan.style = 'color: green';
+        // })
+
     
         this.labelInputHeader = document.createElement('label');
         this.labelInputHeader.textContent = 'Header';
@@ -28,7 +69,7 @@ class NewNoteScreen extends Screen {
         this.form.appendChild(this.labelTextarea);
     
         this.inputText = document.createElement('textarea');
-        this.inputText.id='noteContent'; 
+        this.inputText.id = 'noteContent'; 
         this.inputText.setAttribute('type', 'text', 'name', 'noteContent');
         this.form.appendChild(this.inputText);
     
@@ -45,7 +86,40 @@ class NewNoteScreen extends Screen {
             e.preventDefault();
             this.saveFormToObject();
             this.form.reset();
-        });                      
+        });              
+
+        this.btnForBold.addEventListener('click', (e) =>{
+            // this.contentSpan = document.getElementsByTagName('span');
+            // this.contentSpan.style = 'color: green';
+            this.contentText = document.getElementById('noteContent');
+            this.contentText.style = 'font-weight: bold';
+        });
+
+        this.btnForUnderline.addEventListener('click', (e) =>{
+            // this.contentSpan = document.getElementsByTagName('span');
+            // this.contentSpan.style = 'color: green';
+            this.contentText = document.getElementById('noteContent');
+            this.contentText.style = 'text-decoration: underline';
+        });
+
+        this.btnForSmall.addEventListener('click', (e) =>{
+            // this.contentSpan = document.getElementsByTagName('span');
+            // this.contentSpan.style = 'color: green';
+            this.contentText = document.getElementById('noteContent');
+            this.contentText.style = 'font-size: 14px';
+        });
+
+        this.btnForBig.addEventListener('click', (e) =>{
+            // this.contentSpan = document.getElementsByTagName('span');
+            // this.contentSpan.style = 'color: green';
+            this.contentText = document.getElementById('noteContent');
+            this.contentText.style = 'font-size: 22px';
+        });
+
+        
+
+            
+        
      
     }
 }
